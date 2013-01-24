5b9d24c3df133feab55ae2b0876d10d70f86e83e
Jun Lin <linjunpop@gmail.com>
2013-01-24 20:35:32 +0800

    init

---
+++ app/assets/javascripts/ace.js.coffee
@@ -0,0 +1,4 @@
+$ ->
+  $('[data-behavior~=json-editor]').ace
+    theme: 'xcode'
+    lang: 'json'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryAceExampleApp::Application.config.secret_token = '44dec986e3ea42e9652317cc926a0be995fb95a1a76721b21823681b9671d4244921677f5c1b4021448411dbfcf6b2bb4b1b96901cbe32813c0dd2d597d94088'
